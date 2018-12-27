167a78e574149cf23390f00939abf4a66b575d4b
cyclamen <kantori321@gmail.com>
2018-12-28 00:05:12 +0900

    Separate model and utils
    * Edit target->content
    * Separate matrix, loss functions from utils, and add them model.py
    ---model.py---
    * Add load_model

---
+++ model.py
@@ -0,0 +1,40 @@
+from keras.aplications import vgg19
+from keras import backend as K
+from utils import *
+from scipy.optimize import fmin_l_bfgs_b
+
+'''
+FUNCTION OF MATRIX CALCULATION AND LOSSES (CONTENT, STYLE)
+'''
+# Gram Matrix for style loss function
+def gram_matrix(x):
+    features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
+    gram = K.dot(features, K.transpose(features))
+    return gram
+
+def content_loss(content, synthesized):
+    return K.sum(K.square(synthesized - content))
+
+def style_loss(style, synthesized, size):
+    style = gram_matrix(style)
+    synthesized = gram_matrix(synthesized)
+    channels = 3
+    return K.sum(K.square(style - synthesized) / (4. * (channels ** 2) * (size ** 2)))
+
+# Load model (VGG19 with imagenet)
+def load_model(content, style, size):
+    target_image = K.constant(preprocessing_image(content))
+    style_image = K.constant(preprocessing_image(style))
+    synthesized_image = K.placeholder((1, 400, size, 3))
+
+    input_tensor = K.concatenate([target_image,
+                                  style_image,
+                                  synthesized_image], axis=0)
+
+    model = vgg19.VGG19(input_tensor=input_tensor,
+                        weights='imagenet',
+                        include_top=False)
+    print('Model loaded')
+    return model, synthesized_image
+
+
--- utils.py
+++ utils.py
@@ -6,14 +6,14 @@ from keras import backend as K
 '''
 FUNCTION OF IMAGE LOADING AND PROCESSING
 '''
-# Load Images (target, style reference)
-def load_images(target_image_path, style_image_path, img_height=400):
-    width, height = load_img(target_image_path).size()
+# Load Images (content, style reference)
+def load_images(content_image_path, style_image_path, img_height=400):
+    width, height = load_img(content_image_path).size()
     img_width = int(width * img_height / height)
-    target_image = load_img(target_image_path, target_size=(img_height, img_width))
+    content_image = load_img(content_image_path, target_size=(img_height, img_width))
     style_image = load_img(style_image_path, target_size=(img_height, img_width))
     size = img_height * img_width
-    return target_image, style_image, size
+    return content_image, style_image, size
 
 
 # Preprocessing & Deprocessing image
@@ -30,21 +30,3 @@ def deprocess_image(x):
     x = x[:, :, ::-1]
     x = np.clip(x, 0, 255).astype('uint8')
     return x
-
-'''
-FUNCTION OF MATRIX CALCULATION AND LOSSES (CONTENT, STYLE)
-'''
-# Gram Matrix for style loss function
-def gram_matrix(x):
-    features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
-    gram = K.dot(features, K.transpose(features))
-    return gram
-
-def content_loss(target, synthesized):
-    return K.sum(K.square(synthesized - target))
-
-def style_loss(style, synthesized, size):
-    style = gram_matrix(style)
-    synthesized = gram_matrix(synthesized)
-    channels = 3
-    return K.sum(K.square(style - synthesized) / (4. * (channels ** 2) * (size ** 2)))
